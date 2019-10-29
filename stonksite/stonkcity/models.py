f961ad2bd6e0cf0815e3ba9c5473a72e18e94d6a
connorcinna <connorcinna@gmail.com>
2019-10-28 21:41:24 -0400

    Revert "cloned original project over"
    This reverts commit 66cb72d815e01a793ccf63d30c96ce42b49735e8.

--- stonksite/stonkcity/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from stonks import rt_stock_price
-
-class Images(models.Model): 
-    name = models.CharField(max_length=50) 
-    image_Main_Img = models.ImageField(upload_to='images/') 
